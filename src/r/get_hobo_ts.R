0fa9f3eb4a1f6871fc797bcd7e22f9984d15bec6
Robert W. Burgholzer <rburghol@vt.edu>
2022-12-18 07:35:39 -0500

    exit after warning

--- src/r/get_hobo_ts.R
+++ src/r/get_hobo_ts.R
@@ -5,6 +5,7 @@ library("rjson")
 argst <- commandArgs(trailingOnly=T)
 if (is.na(argst[1])) {
   message("Use: Rscript get_hobo_ts.R featureid entity_type hobo_userid hobo_logger start_date end_date outfile")
+  q(FALSE)
 }
 f_id <- as.integer(argst[1])
 e_type <- argst[2]
